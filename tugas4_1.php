d750a12bed231fd4886692fc676e6a5559e71351
Muhamad Fatah <muhamad.fatah494@gmail.com>
2022-06-29 16:55:00 +0700

    tugas 4 php
    tugas 4 php - niomic

---
+++ tugas4_1.php
@@ -0,0 +1,17 @@
+<?php
+$beli = 8000;
+$jual = 7500;
+$keuntungan = 300;
+$untung = 500;
+$kambing = $untung / $keuntungan;
+
+echo "Harga Beli = Rp $beli <br>";
+echo "Harga Jual = Rp $jual <br>";
+echo "Keuntungan = Rp $keuntungan <br>";
+echo "Berapa Jumlah Kambing Yang dibeli ? <br>";
+echo "Jawaban : ...ekor <br>";
+echo "Keuntungan = Harga Jual - Harga Beli <br>";
+echo "$jual - $beli = - $untung (untung) <br>";
+echo "Jumlah kambing yang dibeli : - $untung / $keuntungan <br>";
+echo "hasilnya = -$kambing => 2 Ekor <br>";
+?>
---
+++ tugas4_2.php
@@ -0,0 +1,12 @@
+<?php
+$saluran = 523;
+$terpakai = 8891;
+$rataRata = $terpakai / $saluran;
+
+echo "Jumlah Saluran = $saluran <br>";
+echo "Jumlah terpakai = $terpakai <br>";
+echo "Rata-rata Pemakaian = ...? <br>";
+echo "Jawaban : ... liter/keluarga <br>";
+echo "Jumlah terpakai / Jumlah saluran = Rata-rata pemakain <br>";
+echo "$terpakai / $saluran = $rataRata Liter/keluarga"
+?>
